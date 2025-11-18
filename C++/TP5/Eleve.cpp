1c658d6c7cd89a4cea15f819e306a63ef7a40385
VEAULIN <veaulinest@gmail.com>
2025-11-18 10:25:31 +0100

    debut eleve3

---
+++ C++/TP5/Eleve.cpp
@@ -0,0 +1,17 @@
+#include "Personne.h"
+#include "Eleve.h"
+#include <iostream>
+#include <string>
+
+
+
+//  Constructeurs
+
+eleve::eleve():{
+    promotion = std::string("");
+    gpa = -1;
+    telephone = std::string("");
+};
+
+
+
